de7a85b127fb8180811854b70874ee004ce0d8bb
Nathan Witemberg <nathan.witemberg@DPB0521.local>
2019-10-03 19:25:34 -0500

    Initial commit

---
+++ testapp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  testapp
+//
+//  Created by Nathan Witemberg on 10/3/19.
+//  Copyright © 2019 Nathan Witemberg. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
